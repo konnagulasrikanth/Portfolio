5aac671c8e0b3b0857e602319b9fce454ed7e9f8
Konnagula Srikanth <73103611+konnagulasrikanth@users.noreply.github.com>
2026-05-04 00:13:29 +0530

    Update Constants.js
    adding updated skillset

--- src/Data/Constants.js
+++ src/Data/Constants.js
@@ -1,348 +1,77 @@
+
 export const Bio = {
-    name: "Srikanth Konnagula",
-    roles: [
-      "Full Stack Developer",
-      ".Net Developer",
-      "Android Developer",
-      "Programmer",
+  name: "Srikanth Konnagula",
+  roles: ["Azure Data Engineer"],
+  description:
+    "Azure Data Engineer with experience in building scalable data ingestion pipelines and processing large datasets using PySpark and SQL. Skilled in Azure Databricks, Azure Data Factory, and Delta Lake.",
+  github: "https://github.com/konnagulasrikanth",
+  resume:
+    "https://drive.google.com/file/d/179k3m0JXWsL_C-sYPtCutoJEOELZ_1gG/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/konnagula-srikanth/",
+};
+
+export const skills = [
+  {
+    title: "Programming",
+    skills: [{ name: "Python" }, { name: "SQL (MySQL)" }, { name: "PySpark" }],
+  },
+  {
+    title: "Cloud",
+    skills: [
+      { name: "Azure Databricks" },
+      { name: "Azure Data Factory" },
+      { name: "Azure Data Lake" },
+      { name: "Delta Lake" },
     ],
-    description:
-      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/konnagulasrikanth",
-    resume:
-      "https://drive.google.com/file/d/179k3m0JXWsL_C-sYPtCutoJEOELZ_1gG/view?usp=drive_link",
-    linkedin: "https://www.linkedin.com/in/konnagula-srikanth/",
-    twitter: "https://twitter.com/KonnagulaS",
-    insta: "https://www.instagram.com/konnagula_srikanth/",
-    facebook: "https://www.facebook.com/people/Srikanth-Konnagula/pfbid02bN9QwViJpvLgnDmWJCyAGvuQo4PVpYqZnK6NgfrNyjyqa2MAKqoYs46StgbiaFBGl/",
-  };
-  
-  export const skills = [
-    {
-      title: "Frontend",
-      skills: [
-        {
-          name: "C#",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/C_Sharp_Logo_2023.svg.png?alt=media&token=8a28161f-69e9-480c-942f-6628e04ff2fa",
-        },
-        {
-          name: "ASP.NET",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/C_Sharp_Logo_2023.svg.png?alt=media&token=8a28161f-69e9-480c-942f-6628e04ff2fa",
-        },
-        {
-          name: "JAVA",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/Java_programming_language_logo.svg.png?alt=media&token=37545735-3fbc-47d3-bfc0-0071070a38bf",
-        },
-        {
-          name: "React Js",
-          image:
-            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
-        },
+  },
+  {
+    title: "Databases",
+    skills: [{ name: "SQL Server (SSMS)" }, { name: "SSAS" }],
+  },
+  {
+    title: "Scheduling",
+    skills: [{ name: "Autosys" }],
+  },
+  {
+    title: "Tools",
+    skills: [{ name: "Azure DevOps (CI/CD)" }, { name: "Git" }],
+  },
+  {
+    title: "Operating Systems",
+    skills: [{ name: "Windows" }],
+  },
+  {
+    title: "Other",
+    skills: [{ name: "Advanced Excel" }],
+  },
+];
 
-        {
-          name: "HTML",
-          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
-        },
-        {
-          name: "CSS",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
-        },
-        {
-          name: "JavaScript",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
-        },
-        {
-          name: "Bootstrap",
-          image:
-            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
-        },
-      ],
-    },
-    {
-      title: "Backend",
-      skills: [
-        {
-          name: "MySQL",
-          image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-        },
-        {
-          name: "ASP.NET Core Web API",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/C_Sharp_Logo_2023.svg.png?alt=media&token=8a28161f-69e9-480c-942f-6628e04ff2fa",
-        },
-        {
-          name: "Node Js",
-          image: "https://nodejs.org/static/images/logo.svg",
-        },
-   
-        {
-          name: "Python",
-          image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
-        },
-    
-        {
-          name: "Firebase",
-          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
-        },
-      ],
-    },
-    {
-      title: "Android",
-      skills: [
-        {
-          name: "Java",
-          image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
-        },
-        {
-          name: "XML",
-          image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
-        },
-        {
-          name: "Android Studio",
-          image:
-            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
-        },
-      ],
-    },
-    
-    {
-      title: "Others",
-      skills: [
-        {
-          name: "Git",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/logo%402x.png?alt=media&token=daec8b2b-a12c-4f6a-bddd-57550e4000c5",
-        },
-        {
-          name: "GitHub",
-          image:
-            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-        },
-        {
-          name: "VS Code",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
-        },
-        {
-          name: "Visual Studio",
-          image:
-            "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/Product-Icon.svg?alt=media&token=170af574-e95d-45f6-ad42-10f36438f2a5",
-        },
-      ],
-    },
-  ];
-  
-  export const experiences = [
-    {
-      id: 0,
-      img: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/icon.png?alt=media&token=5b6edfdb-c672-4543-bf00-58e27f469a0f",
-      role: ".Net Developer",
-      company: "Infinite Computer Solutions Pvt. Ltd.",
-      date: "Aug 2023 - Present",
-      desc: "Working on Desktop Applications ,Console Applications ,WebApplications using .net and ReactJs",
-      skills: [
-        "C#",
-        "ASP.NET",
-        "ASP.NET CORE",
-        "MVC",
-        "MVC CORE",
-        "WPF",
-        "WPF(.NET Framework)",
-        "React Js",
-        "HTML",
-        "JavaScript",
-        "CSS",
-        "Bootstrap",
-        "Java",
-        "MYSQL",
-        "SQL",
+export const experiences = [
+  {
+    id: 0,
+    role: "Data Engineer",
+    company: "Infinite Computer Solutions",
+    date: "Aug 2023 - Present",
+    desc: "Healthcare Project (Client: Molina Healthcare)",
+    points: [
+      "Gathered requirements from stakeholders and implemented business logic using Python and PySpark transformations",
+      "Built scalable data ingestion pipelines to process multiple file formats into Azure Data Lake",
+      "Automated table creation from CSV files improving efficiency and reducing manual effort",
+      "Utilized Azure Databricks notebooks for data transformation and processing workflows",
+      "Designed and managed workflows, scheduled jobs, and configured alerts in Databricks",
+      "Leveraged Databricks Auto Loader to efficiently ingest and process large-scale data into Delta Tables",
+      "Implemented error handling mechanisms to improve pipeline reliability",
+      "Developed Python-based data validation scripts for data quality checks",
+      "Wrote optimized SQL queries improving data processing efficiency by 50%",
+    ],
+  },
+];
 
-
-      ],
-      doc: "",
-    },
-    {
-      id: 1,
-      img: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/isro.jfif?alt=media&token=cc22d58b-93ec-4c97-9ae6-750eff0c8e32",
-      role: "Android Developer",
-      company: "NRSC Student Intern",
-      date: "Feb 10 2023 - Jun 10 2023",
-      desc: "Worked on the developing the Mobile Application Development",
-      skills: [
-        "Java",
-        "XML",
-        "Android Studio",
-      ],
-      doc: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/nrsc%20certificate_page-0001.jpg?alt=media&token=3206894a-49d5-4b90-8f68-3861829f5385",
-    },
-    {
-      id: 2,
-      img: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/logo.png?alt=media&token=7e076220-59ab-4705-98c9-1e24b680ae18",
-      role: "AWS Academy Cloud Architect ",
-      company: "Amazon",
-      date: "Issued on 24/12/2021",
-      desc: "Gained hands-on experience in designing, implementing, and managing scalable, secure, and cost-effective cloud solutions using AWS services, following industry best practices.",
-      skills: [
-        "Service Models(IaaS,PaaS,SaaS)",
-        "Deployment Models(public,private,hybrid)",
-      ],
-      doc: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/architect_page-0001.jpg?alt=media&token=9a33bd6f-68f6-418a-86f6-7d18e3e1aa7e",
-    },
-    {
-      id: 3,
-      img: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/logo.png?alt=media&token=7e076220-59ab-4705-98c9-1e24b680ae18",
-      role: "AWS Academy Cloud Foundations",
-      company: "Amazon",
-      date: "Issued on 27/11/2021",
-      desc: "Acquired foundational knowledge and hands-on experience with core AWS services and cloud computing concepts, focusing on scalability, security, and cost-efficiency.",
-      skills: [
-        "Service Models(IaaS,PaaS,SaaS)",
-        "Deployment Models(public,private,hybrid)",
-      ],
-      doc: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/foundations_page-0001.jpg?alt=media&token=fad318b3-8b94-45f9-b1ee-2c43a31eefc6",
-    },
-   
-  ];
-  
-  export const education = [
-    {
-      id: 0,
-      img: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/college%20icon.jfif?alt=media&token=03839d55-32f6-49fb-b159-43e711db1f95",
-      school: "Sphoorthy Engineering College, Hyderabad",
-      date: "Oct 2019 - Sep 2023",
-      grade: "6.71 CGPA",
-      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Sphoorthy Engineering College, Hyderabad. I have completed 8 semesters and have a CGPA of 6.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the IEDC Student Club  at SPHN, where I am learning and working on exciting projects with a team of talented developers.",
-      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
-    },
-    {
-      id: 1,
-      img: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/interds.jfif?alt=media&token=98982b78-7cb3-43d1-bbd5-0390f8a645ef",
-      school: "Triveni Junior College, Achampet",
-      date: "Apr 2017 - Apr 2019",
-      grade: "92.26%",
-      desc: "I completed my class 12 high school education at Triveni Junior College, Achempet, where I studied Maths, Physics with Chemistry.",
-      degree: "ISC(XII), MPC(Maths, Physics with Chemistry)",
-    },
-    {
-      id: 2,
-      img: "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/tenth.jpg?alt=media&token=7a628d70-b54a-4073-8a16-88aed3043182",
-      school: "Zilla Parishad High School(Boys), Achampet",
-      date: "Apr 2016 - Apr 2017",
-      grade: "78.0%",
-      desc: "I completed my class 10 education at Zilla Parishad High School, Achampet.",
-      degree: "SSC(X)",
-    },
-  ];
-  
-  export const projects = [
-    {
-      id: 9,
-      title: "Training Management System",
-      date: "Nov 23 - Dec 23",
-      description:
-        "Developed a comprehensive application training management system using React.js for the front end and ASP.NET Core Web API for the backend. The system features distinct modules for HR, Managers, and Employees, facilitating streamlined training management, performance tracking, and reporting. The HR module manages training programs and employee records, the Manager module tracks employee progress and assigns tasks, and the Employee module provides access to training materials and progress reports.",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/managemen.jfif?alt=media&token=80c551d8-14cb-464c-b880-a65f941b8808",
-      tags: [
-        "React Js",
-        "ASP.NET Core Web API",
-      ],
-      category: "web app",
-      github: "https://github.com/konnagulasrikanth/TrainingManagementSystem",
-      webapp: "https://github.com/konnagulasrikanth/TrainingManagementSystem",
-    },
-    {
-      id: 0,
-      title: "Project Estimation Tool",
-      date: "Jan 24 - Mar 24",
-      description:
-        "The project's main objective is to implement the “Project Estimation” tool, providing an adaptable approach for accurately estimating the costs and requirements of upcoming projects by project managers and senior management. ",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/Screenshot%20(79).png?alt=media&token=2c993c81-d182-427a-b3b5-18171c7970d1",
-      tags: ["C#", "WPF", "Desktop Application"],
-      category: ".net applications",
-      github: "https://github.com/konnagulasrikanth/ProjectEstimationTool",
-      webapp: "https://github.com/konnagulasrikanth/ProjectEstimationTool",
-    },
-    {
-      id: 1,
-      title: "Folder Structure Creator",
-      date: "May 24 - Jun 24 ",
-      description:
-        "Developed an automated folder management system using a .NET desktop application with WPF (.NET Framework). The application creates and maintains folder structures based on a predefined schema, moves folders and files seamlessly, and ensures data integrity by automatically zipping and archiving deleted folders to a separate location. This system improved organizational efficiency and reduced manual effort in file management tasks",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/Screenshot%20(77).png?alt=media&token=d86148a0-e840-4c89-8857-b09a63689f5b",
-      tags: [
-        "Desktop Application",
-        "WPF(.net framework)",
-      ],
-      category: ".net applications",
-      github: "https://github.com/konnagulasrikanth/FileManager",
-      webapp: "https://github.com/konnagulasrikanth/FileManager",
-    },
-    {
-      id: 2,
-      title: "Smart Cart",
-      date: "Jan 2021 - April 2021",
-      description:
-        "An IEDC project featuring an integrated load cell for accurate weight measurement and error handling, enabling seamless billing directly within the cart to simplify the shopping experience.",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/smart%20cart.jfif?alt=media&token=7688e9cf-415c-4c14-97e2-a6be61c0c23e",
-      tags: ["Python", "Arduino","NVDIA"],
-      category: "web app",
-      github: "https://github.com/konnagulasrikanth/",
-      webapp: "https://github.com/konnagulasrikanth/",
-      // member: [
-      //   {
-      //     name: "Srikanth Konnagula",
-      //     img: "",
-      //     linkedin: "https://www.linkedin.com/in/konnagula-srikanth/",
-      //     github: "https://github.com/konnagula_srikanth/",
-      //   },
-
-      // ],
-    },
-    {
-      id: 3,
-      title: "Android Blood Bank",
-      date: "Dec 2021 - Apr 2022",
-      description:
-        "Developed an APK using Android Studio for the front end and Firebase for the backend, designed to manage and track blood donations efficiently",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/bloodbank.png?alt=media&token=706f7777-ed78-4d65-8346-64b168bb5c2d",
-      tags: ["Java", "XML", "Android Studio"],
-      category: "android app",
-      github: "https://github.com/konnagulasrikanth/",
-      webapp: "https://github.com/konnagulasrikanth/",
-    },
-    {
-      id: 10,
-      title: "Secure Supply Chain Using Blockchain",
-      date: "Apr 2023 - Jun 2023",
-      description:
-        " Implemented a blockchain-based system to enhance the security and transparency of supply chains, ensuring the integrity and traceability of goods throughout the entire supply process.",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/images-41171.appspot.com/o/blockchain.jpg?alt=media&token=e8de78f1-be50-4de6-9168-4a1471b1fda9",
-      tags: ["Blockchain", "Python"],
-      category: "web app",
-      github: "https://github.com/konnagulasrikanth/",
-      webapp: "https://github.com/konnagulasrikanth/",
-    },
-  ];
-  
-  export const TimeLineData = [
-    { year: 2017, text: "Started my journey" },
-    { year: 2018, text: "Worked as a freelance developer" },
-    { year: 2019, text: "Founded JavaScript Mastery" },
-    { year: 2020, text: "Shared my projects with the world" },
-    { year: 2021, text: "Started my own platform" },
-  ];
+export const education = [
+  {
+    id: 0,
+    school: "Sphoorthy Engineering College, Hyderabad",
+    date: "2019 - 2023",
+    degree: "Bachelor of Technology (B.Tech) in Computer Science Engineering",
+  },
+];
